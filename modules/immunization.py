ae52a2e21eea74f378873f49befb0a3f6edb6e3b
JonasPC <sonyogi@hotmail.com>
2018-03-04 16:13:05 +0000

    splittet .py-filen

--- modules/immunization.py
+++ modules/immunization.py
@@ -1,5 +1,4 @@
 
-#Creating the class to do both Exercises
 import numpy as np
 
 class Immunization(object):
@@ -72,36 +71,3 @@ class Immunization(object):
     def investment_to_fv(self, investment, price):
         fv = investment/price
         return fv
-        
-        
-        
-  
-############## EX. 1.1 #################################
-#Creating the instances of GIC's 
-gic1 = Immunization(0.020, 2)
-gic2 = Immunization(0.020, 3)
-
-
-# computing the NPV of future payments
-pv_gic1, pv_gic2 = gic1.pv(100000, 2), gic2.pv(110000, 3)
-pv_liability = pv_gic1 + pv_gic2
-dur_liability = pv_gic1/(pv_gic1+pv_gic2)*gic1.liquidation + pv_gic2/(pv_gic1+pv_gic2)*gic2.liquidation
-#del pv_gic1, pv_gic2
-
-cbb = gic1.cbb_price(0.05, 5)
-frb = gic1.frb_price(4)
-frb = 1
-
-# duration of the two types of bonds
-dur_cbb = gic1.duration(5, bond_type='CBB', list_of_prices = cbb)
-dur_frb = gic1.duration(1, bond_type='ZCB')
-
-# Making weights
-w1 = gic1.weights(dur_liability, dur_cbb, dur_frb)
-
-investment = gic1.investment(w1[0], w1[1], pv_liability)
-result_1_1 = [investment[0]/np.sum(cbb), investment[1]/frb]
-                      
-print(result_1_1)
-
-
