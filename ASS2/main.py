a8594d5237f5325390c7b9e06a6c987d5011016c
JonasPC <sonyogi@hotmail.com>
2018-03-05 22:50:49 +0000

    I gang med opgave 2

--- ASS2/main.py
+++ ASS2/main.py
@@ -34,7 +34,7 @@ print(result_1_1)
 Investment = {'CBB': result_1_1[0], 'FRB': result_1_1[1]}
 df1 = pd.DataFrame(Investment, index=['Amount investable'])
 
-to_latex_table('ex11', df1, directory='ASS2', index=True)
+#to_latex_table('ex11', df1, directory='ASS2', index=True)
 
 # %%
 ############## EX 1.2 #################
@@ -54,4 +54,34 @@ V_assets = np.sum([cbb * cbb_notion, frb * frb_notion])
 Values = {'V assets': V_assets, 'V liabilities': V_liabilities}
 df2 = pd.DataFrame(Values, index=['V'])
 
-to_latex_table('ex12', df2, directory='ASS2', index=True, nr_decimals=1)
+#to_latex_table('ex12', df2, directory='ASS2', index=True, nr_decimals=1)
+
+
+# %%
+############# EX 2.2 #####################
+
+_cbb2 = gic1.cbb_price(0.02, 2)
+cbb2 = np.sum(np.array(_cbb2))
+frb2 = 1
+dur_cbb2 = gic1.duration(2, bond_type='CBB', list_of_prices=_cbb2)
+dur_frb2 = 0.5
+_cbb = gic1.cbb_price(0.05, 5)
+cbb = np.sum(_cbb)
+dur_cbb  = gic1.duration(4, bond_type='CBB', list_of_prices =_cbb[1:])
+#%%
+print('prices', cbb, frb, cbb2, frb2)
+print('durations', dur_cbb, dur_frb, dur_cbb2, dur_frb2)
+# %%
+Values2 = {'CBB_4': cbb, 'CBB_2': cbb2, 'FRB': frb, 'FRB_0.5': frb2}
+df3 = pd.DataFrame(Values2, index=['Prices'])
+Values3 = {'CBB_4': dur_cbb, 'CBB_2': dur_cbb2, 'FRB': dur_frb, 'FRB_0.5': dur_frb2}
+df4 = pd.DataFrame(Values3, index=['Duration'])
+df5 = pd.concat([df3, df4])
+#to_latex_table('ex21', df5, directory='ASS2', index=True, nr_decimals=2)
+#%% Minimizing 
+
+
+
+
+
+
