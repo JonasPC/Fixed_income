152ea12c707911df62034ab0fa1d5a7649a0f216
JakartaLaw <jeppesjohansen@gmail.com>
2018-03-05 12:09:27 +0000

    exercise 12 done

--- ASS2/main.py
+++ ASS2/main.py
@@ -3,11 +3,11 @@ import pandas as pd
 from modules.immunization import Immunization
 from modules.utils import to_latex_table
 
+# %%
 ############## EX. 1.1 #################################
 # Creating the instances of GIC's
 gic1 = Immunization(0.020, 2)
 gic2 = Immunization(0.020, 3)
-
 
 # computing the NPV of future payments
 pv_gic1, pv_gic2 = gic1.pv(100000, 2), gic2.pv(110000, 3)
@@ -17,7 +17,6 @@ dur_liability = pv_gic1 / (pv_gic1 + pv_gic2) * gic1.liquidation + \
 #del pv_gic1, pv_gic2
 
 cbb = gic1.cbb_price(0.05, 5)
-frb = gic1.frb_price(4)
 frb = 1
 
 # duration of the two types of bonds
@@ -36,3 +35,23 @@ Investment = {'CBB': result_1_1[0], 'FRB': result_1_1[1]}
 df1 = pd.DataFrame(Investment, index=['Amount investable'])
 
 to_latex_table('ex11', df1, directory='ASS2', index=True)
+
+# %%
+############## EX 1.2 #################
+# Creating the instances of GIC's
+gic1 = Immunization(0.024, 2)
+gic2 = Immunization(0.024, 3)
+
+cbb = np.sum(gic1.cbb_price(0.05, 5))
+frb = 1.02 * np.exp(-0.024 * 1)
+
+pv_gic1, pv_gic2 = gic1.pv(100000, 2), gic2.pv(110000, 3)
+V_liabilities = pv_gic1 + pv_gic2
+
+cbb_notion, frb_notion = result_1_1[0], result_1_1[1]
+V_assets = np.sum([cbb * cbb_notion, frb * frb_notion])
+
+Values = {'V assets': V_assets, 'V liabilities': V_liabilities}
+df2 = pd.DataFrame(Values, index=['V'])
+
+to_latex_table('ex12', df2, directory='ASS2', index=True, nr_decimals=1)
