2db9f5d00688dec5b3dcf56962e32b5f1554e7a4
JonasPC <sonyogi@hotmail.com>
2018-03-06 15:04:23 +0000

    Vi skal have arbejdet mere med immunization-modulet

--- modules/immunization.py
+++ modules/immunization.py
@@ -1,5 +1,7 @@
 
 import numpy as np
+from scipy.optimize import minimize
+
 
 class Immunization(object):
     """
@@ -47,7 +49,7 @@ class Immunization(object):
             raise Exception('bond_type must be CBB or ZCB')
         return duration
     
-    def convexity(self, maturity, list_of_prices = [], n=1):
+    def convexity(self, maturity, list_of_prices = [], n=1, bond_type = 'CBB'):
         array_of_durations = np.array(range(1, n*(maturity+1)))/n
         array_of_prices = np.array(list_of_prices)
         if bond_type == 'ZCB':
@@ -71,3 +73,37 @@ class Immunization(object):
     def investment_to_fv(self, investment, price):
         fv = investment/price
         return fv
+    
+    @staticmethod
+    def weights_creator(prices, notional):
+       t_w = [prices[i]*notional[i] for i in range(len(prices))]
+       return [w / np.sum(t_w) for w in t_w]
+   
+    @staticmethod
+    def dur(w, dur):
+        list_of_dur = [w[i]*dur[i] for i in range(len(w))]
+        return np.sum(list_of_dur)
+    
+    @staticmethod
+    def convex(w, c):
+        list_of_conv = [w[i]*c[i] for i in range(len(w))]
+        return np.sum(list_of_conv)
+           
+    def practical_immunization(self, objective_func, notional, prices, convexities, durations c_l, d_l):
+        w = self.weigths_creator(prices, notional)
+        c_a = self.convex(w, convexities)
+        d_a = self.dur(w, durations)
+        return [] 
+    
+    @staticmethod
+    def objective_func(N, N_bar):
+        """
+        N and N_bar are to be arrays
+        """
+        if not isinstance(N, np.ndarray) and isinstance(N_bar, np.ndarray) == True:
+            raise Exception('inputs must be numpy array instances')
+        else:
+            objective = np.sum((N-N_bar)**2)
+        return objective
+        
+    
