672352658ea2c349568a093135a05b80886a58bf
JakartaLaw <jeppesjohansen@gmail.com>
2018-03-06 19:38:53 +0000

    added the last for imunization

--- ASS2/main.py
+++ ASS2/main.py
@@ -67,10 +67,18 @@ dur_cbb2 = gic1.duration(2, bond_type='CBB', list_of_prices=_cbb2)
 dur_frb2 = 0.5
 _cbb = gic1.cbb_price(0.05, 5)
 cbb = np.sum(_cbb)
-dur_cbb  = gic1.duration(4, bond_type='CBB', list_of_prices =_cbb[1:])
-#%%
+dur_cbb = gic1.duration(4, bond_type='CBB', list_of_prices=_cbb[1:])
+con_cbb = Immunization.convexity(4, _cbb[1:], bond_type='CBB')
+con_cbb2 = Immunization.convexity(2, _cbb2, bond_type='CBB')
+con_frb = dur_frb**2
+con_frb2 = dur_frb2**2
+cbb2_notion = 0
+frb2_notion = 0
 print('prices', cbb, frb, cbb2, frb2)
 print('durations', dur_cbb, dur_frb, dur_cbb2, dur_frb2)
+print('convexities', con_cbb,  con_frb, con_cbb2, con_frb2)
+print('notionals', cbb_notion, frb_notion, cbb2_notion, frb2_notion)
+
 # %%
 Values2 = {'CBB_4': cbb, 'CBB_2': cbb2, 'FRB': frb, 'FRB_0.5': frb2}
 df3 = pd.DataFrame(Values2, index=['Prices'])
@@ -78,10 +86,33 @@ Values3 = {'CBB_4': dur_cbb, 'CBB_2': dur_cbb2, 'FRB': dur_frb, 'FRB_0.5': dur_f
 df4 = pd.DataFrame(Values3, index=['Duration'])
 df5 = pd.concat([df3, df4])
 #to_latex_table('ex21', df5, directory='ASS2', index=True, nr_decimals=2)
-#%% Minimizing 
 
+dur_liability = pv_gic1 / (pv_gic1 + pv_gic2) * (gic1.liquidation - 1) + \
+    pv_gic2 / (pv_gic1 + pv_gic2) * (gic2.liquidation - 1)
 
+con_liability = pv_gic1 / (pv_gic1 + pv_gic2) * (gic1.liquidation - 1)**2 + \
+    pv_gic2 / (pv_gic1 + pv_gic2) * (gic2.liquidation - 1)**2
 
+not_list = [cbb_notion, frb_notion, cbb2_notion, frb2_notion]
+pri_list = [cbb, frb, cbb2, frb2]
 
+value_assets = np.sum([not_list[i] * pri_list[i] for i in range(len(not_list))])
+print(value_assets)
 
+C_l, D_l = dur_liability, con_liability
+print(C_l, D_l)
 
+b1 = {'P': cbb, 'D': dur_cbb, 'C': con_cbb, 'N': cbb_notion}
+b2 = {'P': frb, 'D': dur_frb, 'C': con_frb, 'N': frb_notion}
+b3 = {'P': cbb2, 'D': dur_cbb2, 'C': con_cbb2, 'N': cbb2_notion}
+b4 = {'P': frb2, 'D': dur_frb2, 'C': con_frb2, 'N': frb2_notion}
+
+list_of_bonds = [b1, b2, b3, b4]
+x0 = [0.25, 0.25, 0.25, 0.25]
+opt_sol = Immunization.Practical_immunization(
+    list_of_bonds, C_l, D_l, x0=x0, value_assets=value_assets)
+
+opt_sol
+print(np.sum(opt_sol.x))
+
+opt_sol
--- modules/immunization.py
+++ modules/immunization.py
@@ -8,93 +8,91 @@ class Immunization(object):
     computes duration of liabilities and assets and adjusts assets such that the net duration = 0.
     Computes the investable amout in each asset to immunize portfolio
     """
-    
+
     def __init__(self, interest, liquidation):
-        self.interest = interest #the flat interest rate 
-        self.liquidation = liquidation #time until liquidation of positions, such that contract can be honored
-        
+        self.interest = interest  # the flat interest rate
+        self.liquidation = liquidation  # time until liquidation of positions, such that contract can be honored
+
     def set_interest(self, interest):
         self.interest = interest
-    
+
     def frb_price(self, maturity):
         list_of_cf = []
         counter = 1
         for i in range(maturity):
-            price_cf = (1)*np.exp(-self.interest)
+            price_cf = (1) * np.exp(-self.interest)
             list_of_cf.append(price_cf)
         return price_cf
-    
+
     def cbb_price(self, c, maturity):
         list_of_cf = []
-        for i in range(1, maturity+1):
+        for i in range(1, maturity + 1):
             if i == maturity:
-                price_cf = c*np.exp(-self.interest*i) + 1*np.exp(-self.interest*i)
+                price_cf = c * np.exp(-self.interest * i) + 1 * np.exp(-self.interest * i)
             else:
-                price_cf = c*np.exp(-self.interest*i)
+                price_cf = c * np.exp(-self.interest * i)
             list_of_cf.append(price_cf)
-        return list_of_cf            
-    
-    def pv(self, future_val, y ):
-        pv = future_val*np.exp(-self.interest*y)
+        return list_of_cf
+
+    def pv(self, future_val, y):
+        pv = future_val * np.exp(-self.interest * y)
         return pv
-        
-    def duration(self, maturity, bond_type='CBB', list_of_prices = [], n=1):
-        array_of_durations = np.array(range(1, n*(maturity+1)))/n
+
+    def duration(self, maturity, bond_type='CBB', list_of_prices=[], n=1):
+        array_of_durations = np.array(range(1, n * (maturity + 1))) / n
         array_of_prices = np.array(list_of_prices)
         if bond_type == 'ZCB':
             duration = maturity
         elif bond_type == 'CBB':
-            duration = sum((array_of_prices*array_of_durations)/sum(array_of_prices))
+            duration = sum((array_of_prices * array_of_durations) / sum(array_of_prices))
         else:
             raise Exception('bond_type must be CBB or ZCB')
         return duration
-    
-    def convexity(self, maturity, list_of_prices = [], n=1, bond_type = 'CBB'):
-        array_of_durations = np.array(range(1, n*(maturity+1)))/n
+
+    @staticmethod
+    def convexity(maturity, list_of_prices=[], n=1, bond_type='CBB'):
+        array_of_durations = np.array(range(1, n * (maturity + 1))) / n
         array_of_prices = np.array(list_of_prices)
         if bond_type == 'ZCB':
             convexity = maturity
-        elif bond_type == 'CCB':
-            convexity = sum((array_of_prices*array_of_durations**2)/sum(array_of_prices))
+        elif bond_type == 'CBB':
+            convexity = sum((array_of_prices * array_of_durations**2) / sum(array_of_prices))
         else:
             raise Exception('bond_type must be CBB or ZCB')
         return convexity
-        
+
     def weights(self, duration_liability, duration1, duration2):
-        weigth1 = (duration_liability - duration2)/(duration1 - duration2)
+        weigth1 = (duration_liability - duration2) / (duration1 - duration2)
         weigth2 = 1 - weigth1
         return [weigth1, weigth2]
-    
+
     def investment(self, weigth1, weigth2, pv_liability):
-        investment1 = weigth1*pv_liability
-        investment2 = weigth2*pv_liability 
+        investment1 = weigth1 * pv_liability
+        investment2 = weigth2 * pv_liability
         return [investment1, investment2]
-    
+
     def investment_to_fv(self, investment, price):
-        fv = investment/price
+        fv = investment / price
         return fv
-    
+
     @staticmethod
     def weights_creator(prices, notional):
-       t_w = [prices[i]*notional[i] for i in range(len(prices))]
-       return [w / np.sum(t_w) for w in t_w]
-   
-    @staticmethod
-    def dur(w, dur):
-        list_of_dur = [w[i]*dur[i] for i in range(len(w))]
+        t_w = [prices[i] * notional[i] for i in range(len(prices))]
+        return [w / np.sum(t_w) for w in t_w]
+
+    @classmethod
+    def dur(cls, prices, notional, dur):  # returns scalar
+        w = cls.weights_creator(prices, notional)
+        list_of_dur = [w[i] * dur[i] for i in range(len(w))]
         return np.sum(list_of_dur)
-    
-    @staticmethod
-    def convex(w, c):
-        list_of_conv = [w[i]*c[i] for i in range(len(w))]
+
+    @classmethod
+    def convex(cls, prices, notional, convexities):
+        c = convexities
+        w = cls.weights_creator(prices, notional)
+        list_of_conv = [w[i] * c[i] for i in range(len(w))]
         return np.sum(list_of_conv)
-           
-    def practical_immunization(self, objective_func, notional, prices, convexities, durations c_l, d_l):
-        w = self.weigths_creator(prices, notional)
-        c_a = self.convex(w, convexities)
-        d_a = self.dur(w, durations)
-        return [] 
-    
+
     @staticmethod
     def objective_func(N, N_bar):
         """
@@ -103,7 +101,59 @@ class Immunization(object):
         if not isinstance(N, np.ndarray) and isinstance(N_bar, np.ndarray) == True:
             raise Exception('inputs must be numpy array instances')
         else:
-            objective = np.sum((N-N_bar)**2)
+            objective = np.sum((N - N_bar)**2)
         return objective
-        
-    
+
+    @staticmethod
+    def bounds_creator(prices, value_assets):
+        return tuple([(0, value_assets * 0.5 / p_i) for p_i in prices])
+
+    @classmethod
+    def Practical_immunization(cls, list_of_dicts, C_l, D_l, value_assets, x0=None):
+
+        pass
+
+        _C = [i['C'] for i in list_of_dicts]
+        _P = [i['P'] for i in list_of_dicts]
+        _D = [i['D'] for i in list_of_dicts]
+        _N = [i['N'] for i in list_of_dicts]
+
+        bnds = cls.bounds_creator(_P, value_assets)
+
+        if x0 is None:
+            x_0 = _N
+
+        def OF(N):
+            return cls.objective_func(N, _N)  # objective_function
+
+        def cond_1(N):
+            # duration assets == durations_liabilites
+            duration_assets = cls.dur(_P, N, _D)
+            duration_liabilities = D_l
+            return duration_liabilities - duration_assets
+
+        def cond_2(N):
+            # Convexity of assets must be greater than of liabilities
+            convexity_assets = cls.convex(_P, N, _C)
+            convexity_liabilites = C_l
+            return convexity_assets - convexity_liabilites  # conc
+
+        def cond_3(N):
+            # Sum of weights must be = 1
+            weights = cls.weights_creator(_P, N)
+            return 1 - np.sum(weights)
+
+        def cond_4(N):
+            # value of assets == value of liabilities
+            new_value_assets = np.sum([N[i] * _P[i] for i in range(len(_P))])
+            return value_assets - new_value_assets
+
+        con1 = {'type': 'eq', 'fun': cond_1}
+        con2 = {'type': 'ineq', 'fun': cond_2}
+        con3 = {'type': 'eq', 'fun': cond_3}
+        con4 = {'type': 'eq', 'fun': cond_4}
+        cons = [con1, con2, con3, con4]
+
+        solution = minimize(OF, x0, method='SLSQP', bounds=bnds, constraints=cons)
+
+        return solution
